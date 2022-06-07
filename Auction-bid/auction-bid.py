0bc93125ccb3ff746c2174ee83bae69617c5380f
Mazcho <77985996+Mazcho@users.noreply.github.com>
2022-06-07 15:25:24 +0700

    Add files via upload

---
+++ Auction-bid/auction-bid.py
@@ -0,0 +1,20 @@
+from art import logo
+from myfunct import find_highest_bidder
+print(logo)
+
+bids = {}
+bidding_finished = False
+
+while not bidding_finished:
+  name = input("What is your name?: ")
+  price = int(input("What is your bid?: $"))
+  bids[name] = price
+  should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+  if should_continue == "no":
+    bidding_finished = True
+    find_highest_bidder(bids)
+  elif should_continue == "yes":
+    bidding_finished = False
+
+        
+        
